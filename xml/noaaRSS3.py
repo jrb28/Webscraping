b5517392d52a734dd67c55cbcda9ad0c6c22de7f
jrb28 <james.bradley@mason.wm.edu>
2021-08-30 20:37:48 -0400

    Update noaaRSS3.py

--- xml/noaaRSS3.py
+++ xml/noaaRSS3.py
@@ -6,7 +6,6 @@ Created on Tue May 31 16:32:02 2016
 """
 
 import requests
-from bs4 import BeautifulSoup as bs
 from lxml import objectify
 
 response = requests.get("http://w1.weather.gov/xml/current_obs/KJGG.xml").content
@@ -17,22 +16,7 @@ print(response)
 
 ''' 2 Parsing Methods '''
 root = objectify.fromstring(response)
-root1 = bs(response, 'lxml')
 
-print("Formatted XML from web site")
-print("===========================")
-print(bs(response, 'lxml').prettify(), '\n')
-
-''' Beautifulsoup Method '''
-print('Root Tag: ', [*root1.html.body.children][0].name, '\n')
-
-print("Selected data from XML structure")
-print("=================================")
-print('Temperature: ',root1.temperature_string.text)
-print('Wind: ', root.wind_string.text)
-print('Heat Index: ', root1.heat_index_string.text)
-
-''' lxml method '''
 print('\n\nRoot Tag: ', root.tag, '\n')
 
 print("Selected data from XML structure")
